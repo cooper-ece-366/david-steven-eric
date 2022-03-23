69dcf50106cb40e4855b11357f25f94800e317a6
ERIC XU <eric.xu@cooper.edu>
2022-03-23 17:39:44 -0400

    merged with dguo-us-12 to add title navigation bar. look at readme for functionality and todos

--- borsa-app/src/App.js
+++ borsa-app/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import useInterval from './useInterval';
-import react_logo from './react-logo.svg';
-import spring_java_logo from './spring-boot-java.jpg';
+import cooper_logo from './logo2.svg';
+import coopercars_logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
+import TextField from "@material-ui/core/TextField";
 
 function getRandomColor() {
   let colorValues = ["red", "blue", "green"];
@@ -10,15 +12,18 @@ function getRandomColor() {
 }
 
 function App() {
-    const apiUrlPrefix = "http://localhost:8080";
-  // const tickers = ['AAPL','AMZN','GOOG','NVDA','INTC','FB','ORCL','NFLX'];
+  function getData(val)
+  {
+    setData(val.target.value)
+    console.warn(val.target.value)
+  }
+  const apiUrlPrefix = "http://localhost:8080";
   const vins = ['1FMCU9GD1HUA30879', '3FA6P0LU1KR242602', '3HGGK5H88KM742051', '5YJ3E1EA5JF098290', '1FTEX1E51KKC66386', '1C4RJFLG1HC603078', '3FA6P0LU1KR101755', 'JTMRWRFV7LJ048851', '1G1RE6E42EU111830', 'SADCJ2BN5HA086947', 'KNMAT2MV3JP608780', '5NPD84LF6KH490922', '5npe34af3jh646547', '2hgfc2f78jh564740', '5YFEPRAE7LP054292', '55SWF4KB5GU142000'];
 
   const [currentVIN, setCurrentVIN] = useState('-');
   const [currentVehicleInfo, setCurrentVehicleInfo] = useState('-');
-
-
-  const vinDelay = 60000; // in milliseconds
+  const [setData, setPrint] = useState(0);
+  const vinDelay = 30000; // in milliseconds
 
 
   App.refreshVehicleInfo = () => {
@@ -50,17 +55,16 @@ function App() {
 
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
-      <img src={react_logo} className="App-logo" alt="react-logo" />
-        <img src={spring_java_logo} className="Platform-logo" alt="spring_java_logo" />
-        <a
-          className="App-link"
-          href="https://github.com/cooper-ece-366/exempli-gratia"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn how to spin React with Java & Spring Boot with API endpoints
-        </a>
+      <img src={cooper_logo} className="App-logo" alt="cooper-logo" />
+
+        <p>
+          Enter VIN of vehicle to add to inventory:
+        </p>
+        <input type="text" onChange={getData}/>
+        <button onClick={()=>setPrint(true)}>Submit</button>
+
         <br></br>
         <button className="button" onClick={App.buttonClicked}>Click to Refresh</button>
 
