c3057a7f2f4e0211d4dc687a6520018acc6e00b4
ERIC XU <eric.xu@cooper.edu>
2022-04-19 22:56:41 -0400

    US-14: TODO-keeps making API calls...

--- coopercars-app/src/components/VehicleDetails.js
+++ coopercars-app/src/components/VehicleDetails.js
@@ -11,14 +11,82 @@ import Button from "@material-ui/core/Button";
 function VehicleDetails()
 {
     const {VIN} = useParams();
-    console.log(VIN);
+    const apiUrlPrefix = "http://localhost:8080";
+    const [currentVehicleInfo, setCurrentVehicleInfo] = useState("");
+    const [currentVehicleImg, setCurrentVehicleImg] = useState("");
+    const [currentVehicleFeatures1, setCurrentVehicleFeatures1] = useState("");
+    const [currentVehicleFeatures2, setCurrentVehicleFeatures2] = useState("");
+    const [currentVehicleFeatures3, setCurrentVehicleFeatures3] = useState("");
+    const [currentVehicleFeatures4, setCurrentVehicleFeatures4] = useState("");
+    const [currentVehicleFeatures5, setCurrentVehicleFeatures5] = useState("");
+
+    // TODO: makes infinite API calls, check this
+    console.log("Refreshing ... %s vehicle ...", VIN);
+    var vehicleApiUrl = apiUrlPrefix.concat("/api/vehicle/getinfo/",VIN);
+    fetch(vehicleApiUrl)
+        .then(response => response.json())
+        .then(data => {
+            setCurrentVehicleImg(data.imgURL);
+            setCurrentVehicleInfo(data.year + " " + data.make + " " + data.model + " " + data.trim + " " + data.series);
+            var hasFCW = "";
+            var hasBSW = "";
+            var hasACC = "";
+            var hasBackup = "";
+
+            if (data.forwColliWarn === "Standard"){
+                hasFCW = "- Is equipped "
+            }
+            else{
+                hasFCW = "- Is not equipped "
+            }
+            if (data.blindSpotWarn == "Standard"){
+                hasBSW = "- Is equipped "
+            }
+            else{
+                hasBSW = "- Is not equipped "
+            }
+            if (data.adaptiveCruiseControl == "Standard"){
+                hasACC = "- Is equipped "
+            }
+            else{
+                hasACC = "- Is not equipped "
+            }
+            if (data.backupCam == "Standard"){
+                hasBackup = "- Is equipped "
+            }
+            else{
+                hasBackup = "- Is not equipped "
+            }
+
+            setCurrentVehicleFeatures1(hasFCW + "with forward collision warning. ");
+            setCurrentVehicleFeatures2(hasBSW + "with blind spot warning. ");
+            setCurrentVehicleFeatures3(hasACC + "with adaptive cruise control. ");
+            setCurrentVehicleFeatures4(hasBackup + "with a backup camera. ");
+            setCurrentVehicleFeatures5("Manufactured in: " + data.plantCountry);
+
+            console.log(data);
+        })
+        .catch(err => {
+            console.log("Cannot connect to API endpoint: %s", vehicleApiUrl);
+        });
+    console.log("Refreshed %s VIN.", VIN);
+
 
     return (
         <div className="App">
             <NavBar />
             <header className="App-header">
 
-                <p>Vehicle details for VIN {VIN} </p>
+                <p>The vehicle for VIN {VIN} is </p>
+                {currentVehicleInfo}<br></br>
+                <div className="car-image">
+                    <img src={currentVehicleImg} height="100"/>
+                </div>
+                {currentVehicleFeatures1}<br></br>
+                {currentVehicleFeatures2}<br></br>
+                {currentVehicleFeatures3}<br></br>
+                {currentVehicleFeatures4}<br></br>
+                {currentVehicleFeatures5}<br></br>
             </header>
         </div>
     );
