c6f3409dbec86705a20cdda5f86557688b6d3b53
David Guo <djon552001@gmail.com>
2022-04-25 11:23:31 -0400

    added spreadsheet vin removal

--- coopercars-app/src/components/RemoveVehicle.js
+++ coopercars-app/src/components/RemoveVehicle.js
@@ -5,14 +5,68 @@ import '../App.css';
 import NavBar from './NavBar'
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import * as XLSX from "xlsx";
 
 function RemoveVehicle()
 {
     const apiUrlPrefix = "http://localhost:8080";
 
+    const [file, setFile] = useState(null);
     const [vin, setVin] = useState("");
     const [status,setStatus] = useState("");
 
+    RemoveVehicle.handleSubmit = () =>{
+        console.log('Button was clicked!');
+        console.log('File name: '+ file.name);
+        const reader = new FileReader();
+        reader.onload = (evt) => { // evt = on_file_select event
+            /* Parse data */
+            const bstr = evt.target.result;
+            const wb = XLSX.read(bstr, {type: 'binary'});
+            /* Get first worksheet */
+            const wsname = wb.SheetNames[0];
+            const ws = wb.Sheets[wsname];
+            /* Convert array of arrays */
+            const data = XLSX.utils.sheet_to_csv(ws, {header: 1});
+            /* Update state */
+            console.log("Data>>> \n" + data);
+            var sheet = wb.Sheets[wb.SheetNames[0]];
+            /* loop through every cell manually */
+            var range = XLSX.utils.decode_range(sheet['!ref']); // get the range
+            for(var R = range.s.r; R <= range.e.r; ++R) {
+                var vi = null;
+                var dealer = null;
+                var sale = null;
+                for (var C = range.s.c; C <= range.e.c; ++C) {
+                    /* find the cell object */
+                    console.log('Row : ' + R);
+                    console.log('Column : ' + C);
+                    var cellref = XLSX.utils.encode_cell({c: C, r: R}); // construct A1 reference for cell
+                    if (!sheet[cellref]) continue; // if cell doesn't exist, move on
+                    var cell = sheet[cellref];
+
+                    if(C==0){
+                        vi = cell.v;
+                        console.log("VIN: " + vi);
+                    }
+                    else if(C==1){
+                        dealer = cell.v;
+                        console.log("Dealer Price: " + dealer);
+                    }
+                    else if(C==2){
+                        sale = cell.v;
+                        console.log("Sale Price: " + sale);
+                    }
+                }
+                console.log(vi + ", " + dealer + ", " + sale);
+                //addVehicleXlsx(vi, dealer, sale);
+                setVin(vi);
+                RemoveVehicle.removeVehicle();
+
+            }
+        }
+        reader.readAsBinaryString(file);
+    }
     RemoveVehicle.removeVehicle = () =>
     {
         const requestOptions = {
@@ -41,6 +95,12 @@ function RemoveVehicle()
         <div className="App">
             <NavBar />
             <header className="App-header">
+                <div>
+                    <h1>Upload Spreadsheet (VIN)</h1>
+                    <input type="file" onChange={(e) => setFile(e.target.files[0])}/>
+                    <button variant="contained" className="button" onClick = {RemoveVehicle.handleSubmit}>Upload</button>
+                </div>
+                <br></br>
                 <p>
                     Enter VIN to remove from Inventory:
                 </p>
